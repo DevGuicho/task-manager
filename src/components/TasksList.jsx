d73fdd7731878ac1041e3f4a0db3c141d792dd9c
Luis Vazquez <luvazpa@gmail.com>
2021-10-24 20:40:45 -0500

    Transition mounting added

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TaskItem from './TaskItem'
 
 import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { tasksRequest } from '../redux/actions/tasksActiosn'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 const TasksList = () => {
+  const dispatch = useDispatch()
   const tasks = useSelector(({ tasks }) => tasks.taskList)
+
+  useEffect(() => {
+    if (tasks.length === 0) {
+      dispatch(tasksRequest())
+    }
+  }, [tasks, dispatch])
   return (
-    <ol>
+    <TransitionGroup component="ol">
       {tasks.map((task) => (
-        <TaskItem key={task.id} {...task} />
+        <CSSTransition key={task.id} timeout={300} classNames="transition">
+          <TaskItem {...task} />
+        </CSSTransition>
       ))}
-    </ol>
+    </TransitionGroup>
   )
 }
 
