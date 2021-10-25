af66e41dc984304a1a8ae2d6fa51551b1f178775
Luis Vazquez <luvazpa@gmail.com>
2021-10-24 20:36:41 -0500

    react heleme added for SEO

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
+import { Toaster } from 'react-hot-toast'
 import AddTask from '../components/AddTask'
 import TasksList from '../components/TasksList'
 import User from '../components/User'
 
 const Home = () => {
   return (
-    <div className="container">
-      <User />
-      <AddTask />
-      <TasksList />
-    </div>
+    <>
+      <Helmet>
+        <title>Home | Task Manager</title>
+      </Helmet>
+      <div className="container">
+        <Toaster />
+        <User />
+        <AddTask />
+        <TasksList />
+      </div>
+    </>
   )
 }
 
