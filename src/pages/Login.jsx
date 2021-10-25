e6efcafc23349aef9dbe437ad80c28cc6361963d
Luis Vazquez <luvazpa@gmail.com>
2021-10-24 20:45:18 -0500

    react helmet titles added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,76 +1,86 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik'
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import { useDispatch } from 'react-redux'
 import { Link, useHistory } from 'react-router-dom'
 
-import { login } from '../redux/actions/usersActions'
+import { loginUser } from '../redux/actions/usersActions'
 import { SignInSchema } from '../utils/schemas/SignUpSchema'
-import SocialButtons from './SocialButtons'
+import SocialButtons from '../components/SocialButtons'
 
 const Login = () => {
   const dispatch = useDispatch()
   const history = useHistory()
 
-  const handleSubmit = ({ email }) => {
+  const handleSubmit = ({ email, password }) => {
     dispatch(
-      login({
-        name: 'Luis Vazquez',
+      loginUser({
         email,
-        token: 'dfsaferfd123fddfasfer345g',
+        password,
       })
     )
     history.replace('/')
   }
 
   return (
-    <div className="container">
-      <div className="login__wrapper">
-        <h1>Login</h1>
-        <Formik
-          initialValues={{ email: '', password: '', remember: false }}
-          onSubmit={handleSubmit}
-          validationSchema={SignInSchema}
-        >
-          <Form action="">
-            <div className="input-control">
-              <label htmlFor="email">Email</label>
-              <Field type="email" name="email" />
-              <ErrorMessage
-                name="email"
-                component="p"
-                className="input-control__error"
-              />
-            </div>
-            <div className="input-control">
-              <label htmlFor="password">Password</label>
-              <Field type="password" name="password" />
-              <ErrorMessage
-                name="password"
-                component="p"
-                className="input-control__error"
-              />
-            </div>
-            <div className="login__options">
-              <label htmlFor="remember">
-                <Field type="checkbox" name="remember" id="remember" />
-                Remember Me
-              </label>
-              <Link to="/">Forgot password ?</Link>
-            </div>
+    <>
+      <Helmet>
+        <title>Login | Task Manager</title>
+      </Helmet>
+      <div className="container">
+        <div className="login__wrapper">
+          <h1>Login</h1>
+          <Formik
+            initialValues={{ email: '', password: '', remember: false }}
+            onSubmit={handleSubmit}
+            validationSchema={SignInSchema}
+          >
+            <Form action="">
+              <div className="input-control">
+                <label htmlFor="email">Email</label>
+                <Field type="email" name="email" />
+                <ErrorMessage
+                  name="email"
+                  component="p"
+                  className="input-control__error"
+                />
+              </div>
+              <div className="input-control">
+                <label htmlFor="password">Password</label>
+                <Field type="password" name="password" />
+                <ErrorMessage
+                  name="password"
+                  component="p"
+                  className="input-control__error"
+                />
+              </div>
+              <div className="login__options">
+                <label htmlFor="remember">
+                  <Field type="checkbox" name="remember" id="remember" />
+                  Remember Me
+                </label>
+                <Link to="/">Forgot password ?</Link>
+              </div>
 
-            <button className="btn-primary btn-login" type="submit">
-              Login
-            </button>
-          </Form>
-        </Formik>
-        <div className="login__hr">
-          <span></span>
-          <p>or sign in with</p>
-          <span></span>
+              <button className="btn-primary btn-login" type="submit">
+                Login
+              </button>
+            </Form>
+          </Formik>
+          <div className="login__hr">
+            <span></span>
+            <p>or sign in with</p>
+            <span></span>
+          </div>
+          <SocialButtons />
+          <div className="login__caption">
+            <Link to="/auth/sign-up" className="login__caption">
+              Don't have an account ?
+            </Link>
+          </div>
         </div>
-        <SocialButtons />
       </div>
-    </div>
+    </>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,70 +1,95 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik'
 import React from 'react'
+import { Helmet } from 'react-helmet'
+import { useDispatch } from 'react-redux'
+import { Link, useHistory } from 'react-router-dom'
+import { registerRequest } from '../redux/actions/usersActions'
 import SignUpSchema from '../utils/schemas/SignUpSchema'
-import SocialButtons from './SocialButtons'
+import SocialButtons from '../components/SocialButtons'
 
 const Register = () => {
+  const history = useHistory()
+  const dispatch = useDispatch()
   const initialValues = {
     name: '',
     email: '',
     password: '',
   }
-  const handleSubmit = (values) => console.log(values)
+  const handleSubmit = (values) => {
+    dispatch(registerRequest(values))
+    history.replace('/')
+    console.log(values)
+  }
   return (
-    <div className="container">
-      <div className="wrapper">
-        <h1>Sign Up</h1>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={SignUpSchema}
-          onSubmit={handleSubmit}
-        >
-          <Form>
-            <div className="input-control">
-              <label htmlFor="name">Name</label>
-              <Field type="text" name="name" placeholder="John Doe" />
-              <ErrorMessage
-                name="name"
-                component="p"
-                className="input-control__error"
-              />
-            </div>
-            <div className="input-control">
-              <label htmlFor="email">Email</label>
-              <Field
-                type="email"
-                name="email"
-                placeholder="johndoe@example.com"
-              />
-              <ErrorMessage
-                name="email"
-                component="p"
-                className="input-control__error"
-              />
-            </div>
-            <div className="input-control">
-              <label htmlFor="password">Password</label>
-              <Field type="password" name="password" />
-              <p className="input-control__caption">At least 8 characters</p>
-              <ErrorMessage
-                name="password"
-                component="p"
-                className="input-control__error"
-              />
-            </div>
-            <button className="btn btn-primary w-full" type="submit">
-              Sign up
-            </button>
-          </Form>
-        </Formik>
-        <div className="login__hr">
-          <span></span>
-          <p>or sign in with</p>
-          <span></span>
+    <>
+      <Helmet>
+        <title>Register | Task Manager</title>
+      </Helmet>
+      <div className="container">
+        <div className="wrapper">
+          <h1>Sign Up</h1>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={SignUpSchema}
+            onSubmit={handleSubmit}
+          >
+            <Form>
+              <div className="input-control">
+                <label htmlFor="name">Name</label>
+                <Field
+                  type="text"
+                  name="name"
+                  placeholder="John Doe"
+                  autoComplete="off"
+                />
+                <ErrorMessage
+                  name="name"
+                  component="p"
+                  className="input-control__error"
+                />
+              </div>
+              <div className="input-control">
+                <label htmlFor="email">Email</label>
+                <Field
+                  type="email"
+                  name="email"
+                  placeholder="johndoe@example.com"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="p"
+                  className="input-control__error"
+                />
+              </div>
+              <div className="input-control">
+                <label htmlFor="password">Password</label>
+                <Field type="password" name="password" />
+                <p className="input-control__caption">At least 8 characters</p>
+                <ErrorMessage
+                  name="password"
+                  component="p"
+                  className="input-control__error"
+                />
+              </div>
+              <button className="btn btn-primary w-full" type="submit">
+                Sign up
+              </button>
+            </Form>
+          </Formik>
+          <div className="login__hr">
+            <span></span>
+            <p>or sign in with</p>
+            <span></span>
+          </div>
+          <SocialButtons />
+          <div className="login__caption">
+            <Link to="/auth/login" className="login__caption">
+              Already have an account?
+            </Link>
+          </div>
         </div>
-        <SocialButtons />
       </div>
-    </div>
+    </>
   )
 }
 
