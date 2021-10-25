d428fedaaebbd4be7fb73e0a6d62bb56643be619
Luis Vazquez <luvazpa@gmail.com>
2021-10-24 20:41:49 -0500

    actions types added

--- src/redux/types/index.js
+++ src/redux/types/index.js
@@ -1,7 +1,14 @@
+export const SET_TASKS = 'SET_TASKS'
 export const ADD_TASK = 'ADD_TASK'
 export const SELECT_TASK = 'SELECT_TASK'
 export const UPDATE_TASK = 'UPDATE_TASK'
 export const DELETE_TASK = 'DELETE_TASK'
 
+export const REGISTER = 'REGISTER'
 export const LOGIN = 'LOGIN'
 export const LOGOUT = 'LOGOUT'
+export const SET_USER = 'SET_USER'
+
+export const SET_LOGIN_ERROR = 'SET_LOGIN_ERROR'
+export const SET_TASK_ERROR = 'SET_TASK_ERROR'
+export const SET_LOADING = 'SET_LOADING'
