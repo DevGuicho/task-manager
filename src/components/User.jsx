3eb6fc34999df9413148932269ef917ecbbfd693
Luis Vazquez <luvazpa@gmail.com>
2021-10-24 20:41:23 -0500

    logout clear state improve

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,14 +3,14 @@ import { HiOutlineLogout } from 'react-icons/hi'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
 import placeholder from '../assets/placeholder.jpg'
-import { logout } from '../redux/actions/usersActions'
+import { logoutUser } from '../redux/actions/usersActions'
 
 const User = () => {
-  const { name } = useSelector(({ user }) => user)
+  const { name } = useSelector(({ user }) => user.user)
   const dispatch = useDispatch()
 
   const handleLogout = () => {
-    dispatch(logout())
+    dispatch(logoutUser())
   }
 
   return (
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,14 +1,25 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import taskReducer from '../reducers/task'
 import usersReducers from '../reducers/usersReducers'
+import { LOGOUT } from '../types'
 
-const reducers = combineReducers({
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const enhancer = composeEnhancers(applyMiddleware(thunk))
+
+const appReducer = combineReducers({
   user: usersReducers,
   tasks: taskReducer,
 })
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+
+const rootReducer = (state, action) => {
+  if (action.type === LOGOUT) {
+    return appReducer(undefined, action)
+  }
+  return appReducer(state, action)
+}
+
+const store = createStore(rootReducer, enhancer)
 
 export default store
