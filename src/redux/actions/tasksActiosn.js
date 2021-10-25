6da45b5bb8910bd9f6a22f6e5cc4e917059ca6a7
Luis Vazquez <luvazpa@gmail.com>
2021-10-24 20:45:44 -0500

    api requests added

--- src/redux/actions/tasksActiosn.js
+++ src/redux/actions/tasksActiosn.js
@@ -1,4 +1,17 @@
-import { ADD_TASK, DELETE_TASK, SELECT_TASK, UPDATE_TASK } from '../types'
+import apiRequest from '../../utils/axios/apiRequest'
+import {
+  ADD_TASK,
+  DELETE_TASK,
+  SELECT_TASK,
+  SET_TASKS,
+  SET_TASK_ERROR,
+  UPDATE_TASK,
+} from '../types'
+
+export const setTasks = (tasks) => ({
+  type: SET_TASKS,
+  payload: tasks,
+})
 
 export const addTask = (task) => ({
   type: ADD_TASK,
@@ -19,3 +32,73 @@ export const deleteTask = (id) => ({
   type: DELETE_TASK,
   payload: id,
 })
+
+export const setError = (err) => ({
+  type: SET_TASK_ERROR,
+  payload: err,
+})
+
+export const tasksRequest = () => async (dispatch) => {
+  try {
+    const { data } = await apiRequest({
+      method: 'GET',
+      url: '/api/tasks',
+      headers: {
+        Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+      },
+    })
+    dispatch(setTasks(data.data.tasks))
+  } catch (error) {
+    dispatch(setError(error.response.data.message))
+  }
+}
+
+export const addTaskRequest = (task) => async (dispatch) => {
+  try {
+    const { data } = await apiRequest({
+      method: 'POST',
+      url: '/api/tasks',
+      headers: {
+        Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+      },
+      data: { name: task },
+    })
+
+    dispatch(addTask(data.data))
+  } catch (error) {
+    dispatch(setError(error.response.data.message))
+  }
+}
+
+export const updateTaskRequest = (task) => async (dispatch) => {
+  const { id, ...restTask } = task
+  try {
+    const { data } = await apiRequest({
+      method: 'PUT',
+      url: `/api/tasks/${task.id}`,
+      data: restTask,
+      headers: {
+        Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+      },
+    })
+    const taskUpdated = data.data
+    dispatch(updateTask(taskUpdated))
+  } catch (error) {
+    dispatch(setError(error.response.data.message))
+  }
+}
+
+export const deleteTaskRequest = (id) => async (dispatch) => {
+  try {
+    await apiRequest({
+      method: 'DELETE',
+      url: `/api/tasks/${id}`,
+      headers: {
+        Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+      },
+    })
+    dispatch(deleteTask(id))
+  } catch (error) {
+    dispatch(setError(error.response.data.message))
+  }
+}
--- src/redux/actions/usersActions.js
+++ src/redux/actions/usersActions.js
@@ -1,4 +1,17 @@
-import { LOGIN, LOGOUT } from '../types'
+import apiRequest from '../../utils/axios/apiRequest'
+import {
+  LOGIN,
+  LOGOUT,
+  REGISTER,
+  SET_LOADING,
+  SET_LOGIN_ERROR,
+  SET_USER,
+} from '../types'
+
+export const register = (user) => ({
+  type: REGISTER,
+  payload: user,
+})
 
 export const login = (user) => ({
   type: LOGIN,
@@ -8,3 +21,82 @@ export const login = (user) => ({
 export const logout = () => ({
   type: LOGOUT,
 })
+
+const setError = (message) => ({
+  type: SET_LOGIN_ERROR,
+  payload: message,
+})
+
+export const setUser = (user) => ({
+  type: SET_USER,
+  payload: user,
+})
+
+export const setIsloading = () => ({
+  type: SET_LOADING,
+})
+
+export const authUser = () => async (dispatch) => {
+  dispatch(setIsloading())
+  try {
+    const { data } = await apiRequest({
+      method: 'GET',
+      url: '/api/auth',
+      headers: {
+        Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+      },
+    })
+    dispatch(setUser(data.data.user))
+  } catch (error) {
+    console.log(error.response.data.message)
+  }
+}
+
+export const registerRequest = (user) => async (dispatch) => {
+  const { name, email, password } = user
+
+  try {
+    const { data } = await apiRequest({
+      method: 'POST',
+      url: '/api/auth/sign-up',
+      data: {
+        name,
+        email,
+        password,
+      },
+    })
+    const { user, token } = data.data
+    console.log({ user, token })
+
+    window.localStorage.setItem('token', token)
+    dispatch(register(user))
+  } catch (error) {
+    console.log('hola')
+    console.log(error)
+  }
+}
+
+export const loginUser =
+  ({ email, password }) =>
+  async (dispatch) => {
+    try {
+      const { data } = await apiRequest({
+        method: 'POST',
+        url: '/api/auth/sign-in',
+        data: {
+          email,
+          password,
+        },
+      })
+      const { user, token } = data.data
+      window.localStorage.setItem('token', token)
+      dispatch(login(user))
+    } catch (error) {
+      dispatch(setError(error.response.data.message))
+    }
+  }
+
+export const logoutUser = () => (dispatch) => {
+  window.localStorage.removeItem('token')
+  dispatch(logout())
+}
