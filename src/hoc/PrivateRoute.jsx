7c9726a51f04fb253cb8ff7924190b0f0762321c
Luis Vazquez <luvazpa@gmail.com>
2021-10-24 20:42:50 -0500

    auth rendering improve

--- src/hoc/PrivateRoute.jsx
+++ src/hoc/PrivateRoute.jsx
@@ -3,8 +3,8 @@ import { useSelector } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const user = useSelector(({ user }) => user)
-  console.log(user)
+  const { user } = useSelector((state) => state.user)
+
   return (
     <Route {...rest}>
       {user ? <Component /> : <Redirect to="/auth/login" />}
--- src/hoc/PublicRoute.jsx
+++ src/hoc/PublicRoute.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
 
 const PublicRoute = ({ component: Component, ...rest }) => {
-  const user = useSelector(({ user }) => user)
+  const user = useSelector(({ user }) => user.user)
   return <Route {...rest}>{!user ? <Component /> : <Redirect to="/" />}</Route>
 }
 
